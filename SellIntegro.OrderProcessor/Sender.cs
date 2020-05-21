3cf72bc2b0ef495f069c90bb0243e14d225f0558
Szczepaniak <ae1214@ae.ads>
2020-05-21 13:18:35 +0200

    Add project files.

---
+++ SellIntegro.OrderProcessor/Sender.cs
@@ -0,0 +1,29 @@
+﻿using InsERT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellIntegro.OrderProcessor
+{
+    class Sender
+    {
+       
+        private bool Exists(int orderId)
+        {
+            return false;
+        }
+        private void SenddOrderToERP(Order order)
+        {
+            InsERT.GT gt = new InsERT.GT();
+            gt.Produkt = InsERT.ProduktEnum.gtaProduktSubiekt;
+            gt.Serwer = "(local)\\INSERTGT";
+            gt.Baza = "test";
+            gt.Operator = "Kowalski Jan";
+            gt.OperatorHaslo = "";
+            gt.Uzytkownik = "Szef";
+            gt.UzytkownikHaslo = "";
+        }
+    }
+}
